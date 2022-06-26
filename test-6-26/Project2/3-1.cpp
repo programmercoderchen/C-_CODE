af49df79cb07f5482b9049e8fa8404ba71111bb0
programmercoderchen <104686459+programmercoderchen@users.noreply.github.com>
2022-06-26 23:27:40 +0800

    六月二十六号

---
+++ test-6-26/Project2/3-1.cpp
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <string.h>
+
+void print(int arr[], int sz)
+{
+	int i = 0;
+	for (i = 0; i < sz; i++)
+	{
+		printf("%d ", arr[i]);
+	}
+	printf("\n");
+}
+void move(int arr[], int sz)
+{
+	int left = 0;
+	int right = sz - 1;
+	//从左边找偶数
+	while (left < right)
+	{
+		//从左边找偶数
+		while ((left < right) && (arr[left] % 2 == 1))
+		{
+			left++;
+		}
+		//从右边找奇数
+		while ((left < right) && (arr[right] % 2 == 0))
+		{
+			right--;
+		}
+		if (left < right)
+		{
+			int tmp = arr[left];
+			arr[left] = arr[right];
+			arr[right] = tmp;
+		}
+	}
+}
+int main()
+{
+	int arr[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+	int sz = sizeof(arr) / sizeof(arr[0]);
+	move(arr, sz);//实现交换
+	print(arr, sz);//打印数组
+	return 0;
+}
